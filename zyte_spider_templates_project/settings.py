2a03210eb95e15cb751c241150e04e758d2d46d7
Andrey Rakhmatullin <wrar@wrar.name>
2024-03-13 17:03:48 +0500

    Switch to scrapy_zyte_api.Addon.

--- zyte_spider_templates_project/settings.py
+++ zyte_spider_templates_project/settings.py
@@ -12,25 +12,20 @@ SPIDER_MODULES = [
 NEWSPIDER_MODULE = "zyte_spider_templates_project.spiders"
 
 CLOSESPIDER_TIMEOUT_NO_ITEM = 600
-DOWNLOAD_HANDLERS = {
-    "http": "scrapy_zyte_api.ScrapyZyteAPIDownloadHandler",
-    "https": "scrapy_zyte_api.ScrapyZyteAPIDownloadHandler",
+ADDONS = {
+    "scrapy_zyte_api.Addon": 500,
 }
 DOWNLOADER_MIDDLEWARES = {
     "scrapy_poet.InjectionMiddleware": 543,
-    "scrapy_zyte_api.ScrapyZyteAPIDownloaderMiddleware": 1000,
 }
-REQUEST_FINGERPRINTER_CLASS = "scrapy_zyte_api.ScrapyZyteAPIRequestFingerprinter"
 SCHEDULER_DISK_QUEUE = "scrapy.squeues.PickleFifoDiskQueue"
 SCHEDULER_MEMORY_QUEUE = "scrapy.squeues.FifoMemoryQueue"
 SPIDER_MIDDLEWARES = {
-    "scrapy_zyte_api.ScrapyZyteAPISpiderMiddleware": 100,
     "scrapy_poet.RetryMiddleware": 275,
     "scrapy.spidermiddlewares.offsite.OffsiteMiddleware": None,
     "zyte_spider_templates.middlewares.AllowOffsiteMiddleware": 500,
     "zyte_spider_templates.middlewares.CrawlingLogsMiddleware": 1000,
 }
-TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 
 # scrapy-poet
 SCRAPY_POET_DISCOVER = [
@@ -40,6 +35,3 @@ SCRAPY_POET_DISCOVER = [
 SCRAPY_POET_PROVIDERS = {
     "scrapy_zyte_api.providers.ZyteApiProvider": 1100,
 }
-
-# scrapy-zyte-api
-ZYTE_API_TRANSPARENT_MODE = True
