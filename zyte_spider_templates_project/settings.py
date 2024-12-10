2da6ba45626170cb277269b2a8ff9c88fda7f165
PyExplorer <stgmont@gmail.com>
2024-12-10 19:23:36 +0300

    prepare for deploy

--- zyte_spider_templates_project/settings.py
+++ zyte_spider_templates_project/settings.py
@@ -18,19 +18,32 @@ ADDONS = {
 }
 SCHEDULER_DISK_QUEUE = "scrapy.squeues.PickleFifoDiskQueue"
 SCHEDULER_MEMORY_QUEUE = "scrapy.squeues.FifoMemoryQueue"
+SCHEDULER_PRIORITY_QUEUE = "scrapy.pqueues.DownloaderAwarePriorityQueue"
+
 SPIDER_MIDDLEWARES = {
     "scrapy_poet.RetryMiddleware": 275,
     "scrapy.spidermiddlewares.offsite.OffsiteMiddleware": None,
+    "zyte_spider_templates.IncrementalCrawlMiddleware": 45,
+    "zyte_spider_templates.middlewares.OffsiteRequestsPerSeedMiddleware": 49,
+    "zyte_spider_templates.middlewares.OnlyFeedsMiddleware": 108,
+    "zyte_spider_templates.middlewares.TrackNavigationDepthSpiderMiddleware": 110,
+    "scrapy_zyte_api.ScrapyZyteAPISpiderMiddleware": 100,
     "zyte_spider_templates.middlewares.AllowOffsiteMiddleware": 500,
+    "zyte_spider_templates.middlewares.TrackSeedsSpiderMiddleware": 550,
     "zyte_spider_templates.middlewares.CrawlingLogsMiddleware": 1000,
 }
-
 # scrapy-poet
 SCRAPY_POET_DISCOVER = [
     "zyte_spider_templates.pages",
     "zyte_spider_templates_project.pages",
 ]
 
+ITEM_PIPELINES = {"zyte_common_items.pipelines.DropLowProbabilityItemPipeline": 0}
+
+ITEM_PROBABILITY_THRESHOLDS = {
+    "zyte_common_items.items.Article": 0.1,
+    "zyte_common_items.items.Product": 0.2,
+}
 # duplicate-url-discarder
 DUD_ATTRIBUTES_PER_ITEM = {
     "zyte_common_items.Product": [
@@ -45,4 +58,10 @@ DUD_ATTRIBUTES_PER_ITEM = {
         "size",
         "style"
     ],
+    "zyte_common_items.Article": [
+            "canonicalUrl",
+            "headline",
+            "datePublishedRaw",
+            "authors"
+        ],
 }
